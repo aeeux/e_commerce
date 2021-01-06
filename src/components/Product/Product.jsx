626e7c4d72507f0176a8e126a477b95696130049
aeeux <andreaseriksen99@hotmail.com>
2021-01-06 05:09:33 +0100

    update

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons'
 import classes from '*.module.css';
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={product.name} />
-            
+
+            <CardContent>
+                
+                <div className={classes.cardContent}>
+
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+
+                </div>
+
+                <Typography variant="h2" color="textSecondary">
+                    {product.description}
+                </Typography>
+                
+            </CardContent>
+
+            <CardActions disableSpacing className={classes.cardActions}>
+
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+
+            </CardActions>
+
         </Card>
     )
 }
