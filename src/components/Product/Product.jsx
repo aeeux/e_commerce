92a97895ae82cbdcad2a44dbda535e9df9ccd887
aeeux <andreaseriksen99@hotmail.com>
2021-01-06 05:36:38 +0100

    update

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/icons'
-import classes from '*.module.css';
+import { AddShoppingCart } from '@material-ui/icons';
+
+import useStyles from './styles';
 
 const Product = ({ product }) => {
+    const classes = useStyles();
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
 
             <CardContent>
-                
+
                 <div className={classes.cardContent}>
 
                     <Typography variant="h5" gutterBottom>
@@ -22,7 +25,7 @@ const Product = ({ product }) => {
 
                 </div>
 
-                <Typography variant="h2" color="textSecondary">
+                <Typography variant="body2" color="textSecondary">
                     {product.description}
                 </Typography>
                 
