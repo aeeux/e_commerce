4506e5d77c9311cbd1dece730765d4311579b994
aeeux <andreaseriksen99@hotmail.com>
2021-01-06 09:46:48 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logojuice.png';
 import useStyles from './styles';
@@ -13,13 +14,13 @@ const Navbar = ({ totalItems }) => {
             
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to="/cart" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Frankly Juice" height="25px" className={classes.image} />
                         Frankly Juice
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
